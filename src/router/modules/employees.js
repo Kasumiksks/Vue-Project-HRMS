a146e932c06c5c616e6c10b99b4463958658f948
kasumi <zils04@163.com>
2022-06-19 17:02:31 +0800

    del Remove unnecessary components

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -1,14 +1,15 @@
 import Layout from '@/layout'
 
 export default { // 员工管理
-  path: '/employees',
-  component: Layout,
+  path: '/employees', // 路径
+  component: Layout, // 组件
   // 设置为layout的二级路由
   children: [
     {
-      path: '', // 作为默认渲染路由
-      name: 'employees',
+      path: '', // 这里当二级路由的path什么都不写的时候 表示当前路由为默认路由直接渲染对应组件
+      name: 'employees', // 给路由规则加一个name
       component: () => import('@/views/employees/employees.vue'),
+      // 路由元信息 , 其实就是存储数据的对象, 我们可以在这里放置一些信息
       meta: { title: '员工管理', icon: 'employees' }
     }
   ]
